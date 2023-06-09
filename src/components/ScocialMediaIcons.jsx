aa1853bc1ddf965a36f4766954692d3d5e8a8057
Muraina020 <glonal-murainaboluwatife@gmail.com>
2023-06-09 08:30:12 +0100

    react-tailwind-project-3

---
+++ src/components/ScocialMediaIcons.jsx
@@ -0,0 +1,46 @@
+import linkedinImage from '../assets/linkedin.png';
+import facebookImage from '../assets/facebook.png';
+import instagramImage from '../assets/instagram.png';
+import twitterImage from '../assets/twitter.png';
+
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500 h-[28px] w-[28px]"
+        href="https://www.linkedin.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+       <img alt="linkedin-link" src={linkedinImage} />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+       <img alt="facebook-link" src={facebookImage} />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500 "
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+       <img alt="instagram-link" src={instagramImage} />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500 "
+        href="https://www.twitter.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+       <img alt="twitter-link" src={twitterImage} />
+      </a>
+
+        </div>
+    )
+
+}
+export default SocialMediaIcons;
---
+++ src/scenes/Footer.jsx
@@ -0,0 +1,21 @@
+import SocialMediaIcons from "../components/ScocialMediaIcons";
+
+const Footer = () => {
+  return (
+    <footer className="h-64 bg-red pt-10 mt-21 md:mt-10">
+      <div className="w-10/12 mx-auto">
+        <SocialMediaIcons />
+        <div className="md:flex justify-center md:justify-between text-center ">
+          <p className="font-playfair font-semibold text-2xl text-yellow">
+            JANE ESPER
+          </p>
+          <p className="font-playfair text-md text-yellow">
+            ©2022 ESPER. All Rights Reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
